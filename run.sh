3158a474ca6d541fe9755ea5782dcf220bb35148
jakeelwes <me@jakeelwes.com>
2017-04-29 22:24:10 +0000

    new

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 inotifywait -m sounds -e create -e moved_to |
     while read path action file; do
         echo "The file '$file' appeared in directory '$path' via '$action'"
-	aplay sounds/$file
+
     done
+
+
 
 
 
