8ac1c4e148aafa2bf8c95afd11bca1678a51672a
jakeelwes <jake.elwes@blueyonder.co.uk>
2017-09-02 17:52:06 +0100

    script

---
+++ conf/script.sh
@@ -0,0 +1,8 @@
+cp ~/.asoundrc ~/.asoundrcB
+cp ~/.bashrc ~/.bashrcB
+cp /etc/default/console-setup /etc/default/console-setupB
+cp /etc/kbd/config /etc/kbd/configB
+mv asoundrc ~/.asoundrc
+mv bashrc ~/.bashrc
+mv console-setup /etc/default/console-setup
+mv kbd__-config /etc/kbd/config
