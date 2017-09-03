a39002b3d90e92978396557229fb22cb9d2fcd70
jakeelwes <jake.elwes@blueyonder.co.uk>
2017-09-03 17:04:56 +0100

    sa

---
+++ readme.md
@@ -0,0 +1,32 @@
+STEPS -
+Copy config.txt (enable 7" screen)
+Make sure connected first - add network
+`sudo nano /etc/wpa_supplicant/wpa_supplicant.conf`
+
+Then do GIT run script
+ ```
+ sudo apt-get update
+ apt-get install git
+ git clone https://github.com/jakeelwes/DigitalWhisper
+ cd DigitalWhisper/
+ sh script.sh
+
+ apt-get install nodejs npm
+ ln -s /usr/bin/nodejs /usr/sbin/node
+ apt-get install espeak
+ cd DigitalWhispers/
+ npm install forever -g
+ sudo reboot
+ ```
+
+ pw3m, autologin, ssh -
+ `sudo raspi-config`
+ No black out
+ `consoleblank=0 end of /boot/cmdline.txt`
+ Volume up
+ `alsamixer`
+ <!-- Add keyboard for boot
+ `sudo crontab -e`
+ @reboot sleep 5 && /bin/echo -e 'connect A0:00:00:00:2C:F2 \n quit \n' | bluetoothctl -->
+Dataplicity
+ `curl https://www.dataplicity.com/..........py | sudo python`
--- script.sh
+++ script.sh
@@ -1,25 +1,4 @@
-# MANUAL THINGS TO DO
-# --- config.txt (enable 7" screen)
-# --- make sure connected first - add network
-# sudo nano /etc/wpa_supplicant/wpa_supplicant.conf
-# --- pw3m, autologin, ssh -
-# sudo raspi-config
-# --- no black out
-# consoleblank=0 end of /boot/cmdline.txt
-# --- volume up
-# alsamixer
-# --- Add keyboard for boot
-# sudo crontab -e
-# @reboot sleep 5 && /bin/echo -e 'connect A0:00:00:00:2C:F2 \n quit \n' | bluetoothctl
 
-sudo apt-get update
-apt-get install git
-apt-get install nodejs npm
-ln -s /usr/bin/nodejs /usr/sbin/node
-apt-get install espeak
-git clone https://github.com/jakeelwes/DigitalWhisper
-cd DigitalWhispers/
-npm install forever -g
 
 
 # Config Files
@@ -35,8 +14,3 @@ mkdir /etc/kbd
 cp kbd__-config /etc/kbd/config
 mkdir /usr/lib/ladspa/
 cp tap_reverb.so /usr/lib/ladspa/tap_reverb.so
-
-sudo reboot
-
-# sudo apt-get update
-# curl https://www.dataplicity.com/..........py | sudo python
